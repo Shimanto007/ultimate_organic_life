368fe115649f9662a3bf150e0b34e0099720c40b
atreeboot <shimantoray007@gmail.com>
2022-11-30 16:53:27 +0600

    52-splitting app to widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
-  final questions = const [
+  final _questions = const [
     {
       'questionText': 'What is your favorite color?',
       'answers': ['black', 'red', 'green', 'white'],
@@ -33,34 +33,29 @@ class _MyAppState extends State<MyApp> {
   ];
 
   void _answerQuestion() {
-
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
-    if(_questionIndex < questions.length) {
+    if (_questionIndex < _questions.length) {
       print('we have more qeustion');
+    } else {
+      print('No more question');
     }
   }
 
   Widget build(BuildContext context) {
-
-
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Ultimate Organic Life'),
         ),
-        body: Column(
-          children: [
-            Question(
-                questions[_questionIndex]['questionText'] as String),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
       ),
     );
   }
